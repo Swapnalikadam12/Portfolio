01bf2e0a0be3af3c88be311341908d1be2d64d7b
Swapnalikadam12 <kadambswapnali@gmail.com>
2022-08-14 21:42:35 +0530

    navbar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -42,7 +42,7 @@ const NavBar = () => {
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-black dark:text-gray-100 hover:scale-105 duration-200"
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 dark:text-gray-200"
           >
             <Link to={link} smooth duration={500}>
               {link}
@@ -58,7 +58,7 @@ const NavBar = () => {
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gray-200 text-black-400 dark:bg-gradient-to-b from-black to-gray-200 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
           {links.map(({ id, link }) => (
             <li
               key={id}
